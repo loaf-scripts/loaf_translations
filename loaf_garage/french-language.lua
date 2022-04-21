80e720ad1541e73909ee29ae89f2c2531f629974
Loaf <60279520+loaf-scripts@users.noreply.github.com>
2022-04-21 20:49:56 +0200

    french garage translation by
    MasterFrazz#3082

---
+++ loaf_garage/french-language.lua
@@ -0,0 +1,24 @@
+Strings = {
+    ["browse_vehicles"] = "Parcourez vos véhicules",
+    ["garage_blip"] = "Garage",
+    ["store_vehicle"] = "Entreposez votre véhicule",
+    ["select_vehicle"] = "Sélectionnez un véhicule",
+    ["impounded"] = "%s (%s) [mis en fourrière]",
+    ["take_out"] = "Sortir %s (%s)",
+    ["already_out"] = "Le véhicule est déjà sorti.",
+    ["not_your_vehicle"] = "Vous n'êtes pas propriétaire de ce véhicule",
+    ["no_vehicles"] = "Vous n'avez aucun véhicule entreposé dans ce garage.",
+    ["invalid_vehicletype"] = "Vous ne pouvez pas entreposer ce type de véhicule dans ce garage.",
+    ["invalid_job"] = "Vous ne pouvez pas entreposer ce véhicule dans ce garage.",
+
+    ["impound_blip"] = "Mettre en fourrière",
+    ["browse_impound"] = "Voir les véhicules mis en fourrière",
+    ["no_money"] = "Vous n'avez pas assez d'argent pour récupérer ce véhicule.",
+    ["no_impounded"] = "Vous n'avez pas de véhicules mis en fourrière ici.",
+
+    ["ping"] = "Votre véhicule a reçu un ping pendant 30 secondes",
+
+    ["browsing_takeout"] = "~INPUT_FRONTEND_RDOWN~ Conduire ~h~%s~h~ [~b~%s~s~]",
+    ["browsing_impounded"] = "~h~%s~h~ [~b~%s~s~] - ~r~mis en fourrière~s~",
+    ["browsing_tip"] = "%s\nUtiliser ~INPUT_FRONTEND_LEFT~ ~INPUT_FRONTEND_RIGHT~ changer de véhicule\nAppuyez ~INPUT_FRONTEND_RRIGHT~ arrêter de naviguer",
+}
