464838d98fc24f324c93226d0dcf4e5264f896dc
Loaf <60279520+loaf-scripts@users.noreply.github.com>
2022-04-23 23:55:37 +0200

    Polish translation by: Mk#5987

---
+++ loaf_housing/polish-language.lua
@@ -0,0 +1,297 @@
+Strings = {
+    -- BLIPS
+    ["purchase_house_blip"] = "Dom na sprzedaż",
+    ["purchase_apart_blip"] = "Apartament na sprzedaż",
+    ["own_house"] = "Twój dom",
+    ["own_apart"] = "Twój apartament",
+    ["ply_own_house"] = "Kogoś dom",
+    ["ply_own_apart"] = "Kogoś apartament",
+
+    -- MARKERS
+    ["manage_house"] = "Otwórz menu domu",
+    ["manage_apart"] = "Otwórz menu apartamentu",
+    
+    -- PURCHASE HOUSE MENU
+    ["purchase_house"] = "Zakupiono dom #%i za $%i?",
+
+    ["purchase_rent_house"] = "Chcesz kupić czy wynająć dom?",
+    ["purchase_rent_apart"] = "Chcesz kupić czy wynająć apartament?",
+    ["purchase"] = "Kup ($%i)",
+    ["rent"] = "Wynajmij ($%i/week)",
+
+    ["rent_house"] = "Chcesz wynając dom na $%i/tygodni?",
+    ["rent_apart"] = "Chcesz wynając apartament na $%i/tygodni?",
+
+    ["purchase_house"] = "Czy chcesz kupić dom za $%i?",
+    ["purchase_apart"] = "Czy chcesz kupić apartament za $%i?",
+
+    -- MANAGE OWNED HOUSE MENU
+    ["manage_owned_house"] = "Twój dom (#%i)",
+    ["manage_owned_apart"] = "Twój apartament (#%i)",
+
+    ["enter_house"] = "Wejdź do domu",
+    ["enter_apart"] = "Wejdź do apartamentu",
+
+    ["copy_keys"] = "Wyrób dodatkowe kluczyki",
+    ["change_lock"] = "Wymień zamek w drzwiach",
+    ["copied_keys"] = "Wyrobiłeś/aś dodatkowe kluczyki",
+    ["changed_lock"] = "Wymieniłeś/aś zamek w drzwiach",
+
+    ["transfer_house"] = "udostępnij dom",
+    ["transfer_apart"] = "udostępnij apartament",
+    ["who_transfer_house"] = "Dla kogo chcesz udostępnić dom #%i ?",
+    ["who_transfer_apart"] = "Dla kogo chcesz udostępnić apartament #%i ?",
+    ["noone_nearby"] = "Nie ma nikogo w pobliżu.",
+    ["transfer_to"] = "Udostępniono %s (%i)",
+    ["confirm_transfer_house"] = "Udostępnij dom #%i dla %s (%i)?",
+    ["confirm_transfer_apart"] = "Udostępnij apartament #%i dla %s (%i)?",
+    ["transferring_house"] = "Udostępnianie domu",
+    ["transferring_apart"] = "Udostępnianie apartamentu",
+    ["transferred_house"] = "Udostępniłeś/aś dom #%i dla %s (%i).",
+    ["transferred_apart"] = "Udostępniłeś/aś apartament #%i dla %s (%i).",
+    ["other_owns_house"] = "%s On/Ona posiada już dom #%i",
+    ["other_owns_apart"] = "%s On/Ona posiada już apartament #%i",
+
+    ["manage_rent"] = "Sprawdź wynajem",
+    ["rent_balance"] = "Koszt wynajmu: $%i",
+    ["rent_due"] = "Wynajete do: %s",
+    ["deposit_rent"] = "Wpłać pieniądze",
+    ["rent_amount"] = "Wartość do wpłacenia",
+
+    ["depositing_rent"] = "Dodawanie gotówki do wynajmu",
+    ["deposited_rent"] = "Dodano gotówke $%i do wynajmu",
+
+    ["sell_house"] = "Sprzedaj dom za $%s",
+    ["sell_apart"] = "Sprzedaj apartament za $%s",
+    ["confirm_sell_house"] = "Sprzedaj twój dom (#%i) za $%s?",
+    ["confirm_sell_apart"] = "Sprzedaj twój apartament (#%i) za $%s?",
+    ["selling_house"] = "Sprzedawanie domu",
+    ["selling_apart"] = "Sprzedawanie apartamentu",
+    ["sold_house"] = "Dom został sprzedany za $%s",
+    ["sold_apart"] = "Apartament został sprzedany za $%s",
+
+    ["enter_other_house"] = "Wejdź do czyjegoś domu",
+    ["enter_other_apart"] = "Wejdź do czyjegoś apartamentu",
+
+    ["your_house_id"] = "ID twojego domu: %s",
+    ["your_apart_id"] = "ID twojego apartamentu: %s",
+
+    ["enter_house_id"] = "Dom id",
+    ["enter_apart_id"] = "Apartament id",
+
+    ["store_vehicle"] = "Schowaj swój pojazd",
+    ["browse_vehicles"] = "Przeglądaj swoje pojazdy",
+
+    -- UNIQUE OWNED PROPERTY MENU
+    ["enter_the_house"] = "Wejdź do domu",
+    ["enter_the_apart"] = "Wejdź do apartamentu",
+
+    ["knock_on_door"] = "Zapukaj do drzwi",
+    ["lockpick_door"] = "Włam sie",
+
+    -- ENTER / PURCHASE / LOCKPICK 
+    ["house_menu"] = "Menu Domu (#%i)",
+    ["apart_menu"] = "Menu Apartamentu (#%i)",
+
+    ["purchase_rent"] = "Kup / Wynajmij",
+
+    ["no_access"] = "Drzwi są zamknięte.",
+    ["no_spawn"] = "ARGH! nie można tego zrobić. Powiedz włascicielowi serwera Narezowi ze musi Config.SpawnAboveHouse zrobic na true, albo dodać więcej lokalizacji spawnu.",
+    ["knock_door"] = "Czy chcesz zapukać do drzwi?",
+    ["noone_home"] = "Wydaje sie że nikogo nie ma",
+    ["someone_knocking"] = "Ktoś puka do drzwi",
+
+    ["not_config"] = "Tej posiadłości nie ma w konfigu... kurwo 12 letnia czy ty cheatujesz? zaraz cie dopadnie Rick i ci spierze dupe za ten RedEngine",
+    ["max_properties"] = "Już posiadasz maksymalna ilość posiadłości.",
+    ["already_owns"] = "Jest to twoja posiadłość.",
+    ["not_rentable"] = "Tej posiadłości nie można wynająć.",
+    ["no_money"] = "Nie masz wystarczająco pieniędzy",
+    ["someone_owns"] = "Ktoś już kupił tą posiadłość.",
+
+    ["house_not_exist"] = "Nie ma domu o takim id %s",
+    ["apart_not_exist"] = "Nie ma apartemntu o takim id %s",
+
+    -- LOCKPICKING
+    ["no_lockpicks"] = "Nie masz żadnych wytrychów",
+    ["ran_out_lockpicks"] = "Wykorzystałeś/aś wszystkie wytrychy",
+    ["lockpicking_help"] = [[
+Kiedy wytrych spowalnia, To znaczy że prawie dobrze otworzyłeś/aś.
+Lockpicks left: %s
+
+Use ~INPUT_CELLPHONE_LEFT~ ~INPUT_CELLPHONE_RIGHT~ Żeby zamknąć drzwi
+~INPUT_FRONTEND_RDOWN~ Otworzyć
+~INPUT_FRONTEND_RRIGHT~ Anuluj]],
+    ["lockpick_apart"] = "Włam sie do czyjegoś apartamentu",
+    ["lockpick_house"] = "Włam sie do czyjegoś domu",
+    ["lockpicked"] = "Włamałeś sie! Policja została powiadomiona przez sąsiadów.",
+    ["robbery_progress"] = "~r~ALERT~s~\nKtoś sie włamuje!  ~r~waypoint~s~ Gps został wyznaczony (ID: %s)",
+    ["robbery"] = "Włamanie sie na posiadłość (%s)",
+    ["no_cops"] = "Nie ma wystarczająco Policji.",
+
+    -- INSIDE PROPERTY MENU
+    ["exit_apart"] = "Wyjdź z apartamentu",
+    ["exit_house"] = "Wyjdź z domu",
+
+    ["instance_menu_house"] = "Menu domu (#%i)",
+    ["instance_menu_apart"] = "Menu apartamentu (#%i)",
+
+    ["furnish_apart"] = "Umebluj Apartament",
+    ["furnish_house"] = "Umebluj Dom",
+
+    ["manage_furniture"] = "Edytuj ułożenie Mebli",
+    ["select_furniture"] = "Wybierz meble",
+    ["current_furniture"] = "Wybrano: %i",
+    ["furniture_id"] = "ID Mebla",
+    ["remove_selected"] = "Usuń wybrany mebel",
+    ["removed_furniture"] = "Usunięto Mebel",
+
+    ["house_id"] = "Id domu: %s",
+    ["apart_id"] = "Id apartamentu: %s",
+
+    ["entering_house"] = "Wchodzenie do domu",
+    ["entering_apart"] = "Wchodzenie do apartamentu",
+
+    -- STORAGE
+    ["need_key_storage"] = "Musisz mieć kluczyk aby korzystać ze schowka",
+    ["access_storage"] = "Otwórz schowek",
+    ["access_wardrobe"] = "Otwórz szafe",
+    ["choose_storage"] = "Schowek czy szafa?",
+    ["storage"] = "Schowek",
+    ["wardrobe"] = "Szafa",
+    ["deposit_withdraw"] = "Schowaj czy wyciągnij?",
+    ["deposit"] = "Schowaj",
+    ["withdraw"] = "Wyciągnij",
+    ["empty"] = "Pusty",
+    ["dont_have"] = "Nie masz tyle",
+    ["storage_not_enough"] = "Nie ma tyle w schowku",
+    ["dont_have_weapon"] = "Nie masz takiej broni.",
+
+    ["deposit_money"] = "Schowaj pieniądze (<span style='color:lime'>$%s</span>)",
+    ["deposit_dirty"] = "Schowaj brudne pieniądze (<span style='color:red'>$%s</span>)",
+    ["deposit_items"] = "Schowaj przedmiot",
+    ["deposit_weapons"] = "Schowaj broń",
+    ["amount_deposit"] = "Ilość rzeczy do schowania",
+    ["deposited_item"] = "Schowałeś/aś x%i %s",
+    ["deposited_money"] = "Schowałeś/aś $%i",
+    ["deposited_weapon"] = "Schowałeś/aś twój %s",
+
+    ["withdraw_money"] = "Wyciągnij pieniądze (<span style='color:lime'>$%s</span>)",
+    ["withdraw_dirty"] = "Wyciągnij brudne pieniądze (<span style='color:red'>$%s</span>)",
+    ["withdraw_items"] = "Wyciągnij przedmiot",
+    ["withdraw_weapons"] = "Wyciągnij broń",
+    ["amount_withdraw"] = "Ilość do wyciągniecia",
+    ["withdrew_item"] = "Wyciągnąłeś/aś x%i %s",
+    ["withdrew_money"] = "Wyciągnąłeś/aś $%i",
+    ["withdrew_weapon"] = "Wyciągnąłeś/aś twój %s",
+
+    ["cant_carry"] = "Nie możesz tyle nosić.",
+
+    ["inventory"] = "Schowek posiadłości",
+
+    -- WARDROBE
+    ["select_outfit"] = "Wybierz ubranie",
+    ["equip_outfit"] = "Ubierz sie w to ubranie",
+    ["delete_outfit"] = "Wyrzuć te ubranie",
+    ["confirm_delete_outfit"] = "Wyrzucić te ubranie %s?",
+    ["no_outfits"] = "Nie masz tyle ubrań",
+
+    -- FURNISH
+    ["furnishing"] = [[
+Speed: %s ~INPUT_COVER~ ~INPUT_TALK~
+Use ~INPUT_WEAPON_WHEEL_PREV~ ~INPUT_WEAPON_WHEEL_NEXT~ żeby ruszać przedmiotem góra/dół
+Use ~INPUT_ATTACK~ ~INPUT_AIM~ aby obracać przedmiot (Kąt: %s)
+Use ~INPUT_CELLPHONE_LEFT~ ~INPUT_CELLPHONE_RIGHT~ ~INPUT_CELLPHONE_UP~ ~INPUT_CELLPHONE_DOWN~ żeby poruszać przedmiotem
+Press ~INPUT_DETONATE~ Aby teleportować przedmiot do siebie
+Press ~INPUT_FRONTEND_RDOWN~ aby potwierdzić
+Press ~INPUT_FRONTEND_RRIGHT~ aby anulować
+]],
+    ["placing_furniture"] = "Stawianie mebli",
+
+    -- PURCHASE HOUSE NOTIFICATIONS & LOADING
+    ["owns_max"] = "Już posiadasz maksymalna ilość posiadłości dlatego nie możesz zakupić kolejnej.",
+    ["processing_payment"] = "Wykonywanie transakcji....",
+
+    -- PREVIEW
+    ["previewing"] = [[
+~h~Przeglądanie wnętrza~h~ (%i/%i)
+
+Previewing interior %s
+Use ~INPUT_CELLPHONE_LEFT~ ~INPUT_CELLPHONE_RIGHT~ Żeby przegladać inne wnętrza
+Press ~INPUT_FRONTEND_RRIGHT~ Przestań przeglądać.
+]],
+    ["previewing_nohelp"] = [[
+~h~Przeglądanie wnętrza~h~
+
+Press ~INPUT_FRONTEND_RRIGHT~ aby przestać przeglądać
+]],
+    ["preview_property"] = "Zobacz wnętrze",
+    ["select_interior"] = "Wybierz wnętrze",
+
+    -- KEY
+    ["key_label"] = "%s #%i [%s]",
+    ["not_nearby"] = "Musisz być przy drzwiach aby otworzyć/zamknąć.",
+
+    ["locked_apart"] = "Właśnie ~r~locked~s~ apartament ",
+    ["unlocked_apart"] = "Właśnie ~g~unlocked~s~ apartment",
+
+    ["locked_house"] = "Właśnie ~r~locked~s~ dom",
+    ["unlocked_house"] = "Właśnie ~g~unlocked~s~ dom",
+
+    -- FURNITURE STORE
+    ["furniture_blip"] = "Sklep z meblami",
+    ["furniture_marker"] = "Przeglądaj meble",
+
+    ["furniture_is_storage"] = "\nNie możesz przechowywać przedmiotów w tym",
+    ["furniture_browsing"] = [[
+Przeglądanie ~r~%s~s~%s
+Use ~INPUT_REPLAY_BACK~ ~INPUT_REPLAY_ADVANCE~ Żeby zmienić przedmiot(%i/%i)
+Use ~INPUT_REPLAY_FFWD~ ~INPUT_REPLAY_REWIND~ Żeby zmienić kategorie (%i/%i)
+Use ~INPUT_MOVE_LEFT_ONLY~ ~INPUT_MOVE_RIGHT_ONLY~ Żeby obrócić przedmiot
+Use ~INPUT_CURSOR_SCROLL_UP~ ~INPUT_CURSOR_SCROLL_DOWN~ Żeby przybliżyc i oddalic
+Use ~INPUT_MOVE_UP_ONLY~ ~INPUT_MOVE_DOWN_ONLY~ aby obracać kamere prawo lewo
+Press ~INPUT_FRONTEND_RDOWN~ żeby kupić ~b~%s~s~ for ~g~$%s~s~.
+Press ~INPUT_FRONTEND_RRIGHT~ żeby wyjść ze sklepu z meblami]],
+    ["furniture_confirm"] = [[
+Purchase ~b~%s~s~ for ~g~$%s~s~?
+~INPUT_FRONTEND_RDOWN~ Kup
+~INPUT_FRONTEND_RRIGHT~ Anuluj
+]],
+    ["loading_object"] = "Ładowanie obiektu",
+    ["couldnt_load"] = "Nie można załadowac obiektu (%s)",
+    ["furniture_purchase_sell"] = "Sklep z meblami",
+    ["purchase_furniture"] = "Przeglądaj meble",
+    ["resell_furniture"] = "Sprzedaj meble",
+    ["fetching_furniture"] = "Sprawdzanie posiadanych mebli",
+    ["no_furniture"] = "Nie masz żadnych mebli.",
+    ["your_furniture"] = "Twoje meble",
+    ["owned_furniture"] = "%s x%i",
+    ["selling_furniture"] = "Sprzedawanie mebli",
+    ["sold_furniture"] = "Sprzedałeś x%i %s za $%i",
+    ["sell_one"] = "Sprzedaj jeden za $%i",
+    ["sell_all"] = "Sprzedaj wszystkie za $%i",
+    ["purchased_furniture"] = "Kupiłeś %s za $%i",
+
+    -- GENERAL TEXT
+    ["yes"] = "Tak",
+    ["no"] = "Nie",
+    ["locked"] = "Zamknięte",
+    ["unlocked"] = "Otwarte",
+    
+    ["spawn_apart"] = "Apartment #%i",
+    ["spawn_house"] = "Dom #%i",
+
+    ["contact_realtor"] = "Skontaktuj sie z właścicielem",
+
+    -- LOGS
+    ["LOG_backed_up"] = "Zapisano %s's posiadłość (#%i, id %s).\n\nFile location: %s",
+    ["LOG_purchase"] = "Zakupiono posiadłość #%i for $%i",
+    ["LOG_sell"] = "Sprzedano posiadłość #%i for $%i",
+    ["LOG_deposit"] = "schowano x%i %s",
+    ["LOG_withdrew"] = "wyciągnięto x%i %s",
+}
+
+-- ignore this
+setmetatable(Strings, {__index = function(self, key)
+    print("NO KEY", key)
+    return "Error: Missing translation for \""..key.."\""
+end})
