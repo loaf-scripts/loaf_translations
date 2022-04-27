a9a99ce4f9541b3663fe3824da7e36125fd1da09
Loaf <60279520+loaf-scripts@users.noreply.github.com>
2022-04-27 10:49:26 +0200

    Create swedish-language.lua

---
+++ loaf_housing/swedish-language.lua
@@ -0,0 +1,305 @@
+Strings = {
+    -- BLIPS
+    ["purchase_house_blip"] = "Hus till salu",
+    ["purchase_apart_blip"] = "Lägenhet till salu",
+    ["own_house"] = "Ditt hus",
+    ["own_apart"] = "Din lägenhet",
+    ["ply_own_house"] = "Någons hus",
+    ["ply_own_apart"] = "Någons lägenhet",
+
+    -- MARKERS
+    ["manage_house"] = "Öppna husmeny (#%i)",
+    ["manage_apart"] = "Öppna lägenhetsmeny (#%i)",
+    
+    -- PURCHASE HOUSE MENU
+    ["purchase_house"] = "Köp hus #%i för %ikr?",
+
+    ["purchase_rent_house"] = "Vill du köpa eller hyra det här huset?",
+    ["purchase_rent_apart"] = "Vill du köpa eller hyra den här lägenheten?",
+    ["purchase"] = "Köp (%ikr)",
+    ["rent"] = "Hyr (%ikr/vecka)",
+
+    ["rent_house"] = "Vill du hyra huset för %ikr/vecka?",
+    ["rent_apart"] = "Vill du hyra lägenheten för %ikr/vecka?",
+
+    ["purchase_house"] = "Vill du köpa huset för %ikr?",
+    ["purchase_apart"] = "Vill du köpa lägenheten för %ikr?",
+
+    -- MANAGE OWNED HOUSE MENU
+    ["manage_owned_house"] = "Ditt hus (#%i)",
+    ["manage_owned_apart"] = "Din lägenhet (#%i)",
+
+    ["enter_house"] = "Gå in i ditt hus",
+    ["enter_apart"] = "Gå in i din lägenhet",
+
+    ["copy_keys"] = "Kopiera dina nycklar",
+    ["change_lock"] = "Ändra låset",
+    ["copied_keys"] = "Du koperiade nycklarna",
+    ["changed_lock"] = "Du ändrade låset",
+
+    ["transfer_house"] = "Överför huset",
+    ["transfer_apart"] = "Öveför lägenheten",
+    ["who_transfer_house"] = "Vem vill du överföra hus #%i till?",
+    ["who_transfer_apart"] = "Vem vill du överföra lägenhet #%i till?",
+    ["noone_nearby"] = "Ingen i närheten.",
+    ["transfer_to"] = "Överför till %s (%i)",
+    ["confirm_transfer_house"] = "Överför hus #%i till %s (%i)?",
+    ["confirm_transfer_apart"] = "Överför lägenhet #%i till %s (%i)?",
+    ["transferring_house"] = "Överför huset",
+    ["transferring_apart"] = "Överför lägenheten",
+    ["transferred_house"] = "Du överförde hus #%i till %s (%i).",
+    ["transferred_apart"] = "Du överförde lägenhet #%i till %s (%i).",
+    ["other_owns_house"] = "%s äger redan hus #%i",
+    ["other_owns_apart"] = "%s äger redan lägenhet #%i",
+
+    ["manage_rent"] = "Hantera hyran",
+    ["rent_balance"] = "Saldo: $%i",
+    ["rent_due"] = "Förfallodatum: %s",
+    ["deposit_rent"] = "Sätt in pengar",
+    ["rent_amount"] = "Mängd att sätta in",
+
+    ["depositing_rent"] = "Sätter in pengar",
+    ["deposited_rent"] = "Satte in %ikr i saldot",
+
+    ["sell_house"] = "Sälj huset för %skr",
+    ["sell_apart"] = "Sälj lägenheten för %skr",
+    ["confirm_sell_house"] = "Sälj ditt hus (#%i) för $%s?",
+    ["confirm_sell_apart"] = "Sälj din lägenhet (#%i) för $%s?",
+    ["selling_house"] = "Säljer huset",
+    ["selling_apart"] = "Säljer lägenheten",
+    ["sold_house"] = "Hus sålt för $%s",
+    ["sold_apart"] = "Lägenhet såld för $%s",
+
+    ["enter_other_house"] = "Gå in i någons hus",
+    ["enter_other_apart"] = "Gå in i någons lägenhet",
+
+    ["your_house_id"] = "Ditt hus-id: %s",
+    ["your_apart_id"] = "Ditt lägenhets-id: %s",
+
+    ["enter_house_id"] = "Hus-id",
+    ["enter_apart_id"] = "Lägenhets-id",
+
+    ["store_vehicle"] = "Sätt in bil",
+    ["browse_vehicles"] = "Öppna garage",
+
+    -- UNIQUE OWNED PROPERTY MENU
+    ["enter_the_house"] = "Gå in i huset",
+    ["enter_the_apart"] = "Gå in i lägenheten",
+
+    ["knock_on_door"] = "Knacka",
+    ["lockpick_door"] = "Dyrka dörren",
+    ["breach_door"] = "Bryt upp dörren",
+
+    -- ENTER / PURCHASE / LOCKPICK 
+    ["house_menu"] = "Husmeny (#%i)",
+    ["apart_menu"] = "Lägenhetsmeny (#%i)",
+
+    ["purchase_rent"] = "Köp / hyr",
+
+    ["no_access"] = "Dörren är låst.",
+    ["no_spawn"] = "Kunde inte hitta ett spawnställe för interiören. Säg till en administratör att sätta Config.SpawnAboveHouse till true, eller lägga till fler spawnställen.",
+    ["knock_door"] = "Vill du knacka på dörren?",
+    ["noone_home"] = "Ingen är hemma",
+    ["someone_knocking"] = "Någon knackar på dörren",
+
+    ["not_config"] = "Fastigheten finns ej i configen",
+    ["max_properties"] = "Du kan inte äga fler fastigheter.",
+    ["already_owns"] = "Du äger redan den här fastigheten.",
+    ["not_rentable"] = "Du kan ej hyra denna fastigheten.",
+    ["no_money"] = "Du har inte tillräckligt med pengar",
+    ["someone_owns"] = "Någon äger redan den här fastigheten.",
+
+    ["house_not_exist"] = "Det finns inget hus med id %s",
+    ["apart_not_exist"] = "Det finns ingen lägenhet med id %s",
+
+    -- LOCKPICKING
+    ["no_lockpicks"] = "Du har inga dyrkset",
+    ["ran_out_lockpicks"] = "Du har använt alla dina dyrkset",
+    ["lockpicking_help"] = [[
+När låset saktar ner är du nära rätt ställe.
+Dyrkset kvar: %s
+
+Använd ~INPUT_CELLPHONE_LEFT~ ~INPUT_CELLPHONE_RIGHT~ för att rotera låset
+~INPUT_FRONTEND_RDOWN~ lås upp
+~INPUT_FRONTEND_RRIGHT~ avbryt]],
+    ["lockpick_apart"] = "Dyrka någons lägenhet",
+    ["lockpick_house"] = "Dyrka någons hus",
+    ["lockpicked"] = "Du dyrkade dörren.",
+    ["robbery_progress"] = "~r~INFO~s~\nNågon dyrkar en dörr! En ~r~waypoint~s~ har blivit placerad på din GPS (ID: %s)",
+    ["robbery"] = "Inbrott (%s)",
+    ["no_cops"] = "Det är inte tillräckligt med poliser online.",
+
+    -- BREACH DOOR
+    ["breach_apart"] = "Lägenhet att bryta upp dörren på",
+    ["breach_house"] = "Hus att bryta upp dörren på",
+
+    -- INSIDE PROPERTY MENU
+    ["exit_apart"] = "Lämna lägenheten",
+    ["exit_house"] = "Lämna huset",
+
+    ["instance_menu_house"] = "Husmeny (#%i)",
+    ["instance_menu_apart"] = "Lägenhetsmeny (#%i)",
+
+    ["furnish_apart"] = "Möblera lägenheten",
+    ["furnish_house"] = "Möblera huset",
+
+    ["let_someone_in"] = "Släpp in någon",
+    ["let_in"] = "Släpp in %s?",
+
+    ["manage_furniture"] = "Hantera utplacerade möbler",
+    ["select_furniture"] = "Välj möbel",
+    ["current_furniture"] = "Valde: %i",
+    ["furniture_id"] = "Möbel-id",
+    ["remove_selected"] = "Ta bort vald möbel",
+    ["removed_furniture"] = "Tog bort möbeln",
+
+    ["house_id"] = "Hus-id: %s",
+    ["apart_id"] = "Lägenhets-id: %s",
+
+    ["entering_house"] = "Går in i huset",
+    ["entering_apart"] = "Går in i lägenheten",
+
+    -- STORAGE
+    ["need_key_storage"] = "Du behöver en nyckel",
+    ["access_storage"] = "Förråd",
+    ["access_wardrobe"] = "Garderob",
+    ["choose_storage"] = "Förråd eller garderob?",
+    ["storage"] = "Förråd",
+    ["wardrobe"] = "Garderob",
+    ["deposit_withdraw"] = "Sätt in eller ta ut?",
+    ["deposit"] = "Deposit",
+    ["withdraw"] = "Withdraw",
+    ["empty"] = "Empty",
+    ["dont_have"] = "You don't have that much",
+    ["storage_not_enough"] = "There's not that much in storage",
+    ["dont_have_weapon"] = "You don't have that weapon.",
+
+    ["deposit_money"] = "Deposit money (<span style='color:lime'>$%s</span>)",
+    ["deposit_dirty"] = "Deposit dirty money (<span style='color:red'>$%s</span>)",
+    ["deposit_items"] = "Deposit an item",
+    ["deposit_weapons"] = "Deposit a weapon",
+    ["amount_deposit"] = "Amount to deposit",
+    ["deposited_item"] = "You deposited x%i %s",
+    ["deposited_money"] = "You deposited $%i",
+    ["deposited_weapon"] = "You deposited your %s",
+
+    ["withdraw_money"] = "Withdraw money (<span style='color:lime'>$%s</span>)",
+    ["withdraw_dirty"] = "Withdraw dirty money (<span style='color:red'>$%s</span>)",
+    ["withdraw_items"] = "Withdraw an item",
+    ["withdraw_weapons"] = "Withdraw a weapon",
+    ["amount_withdraw"] = "Amount to withdraw",
+    ["withdrew_item"] = "You withdrew x%i %s",
+    ["withdrew_money"] = "You withdrew $%i",
+    ["withdrew_weapon"] = "You withdrew a %s",
+
+    ["cant_carry"] = "You can't carry that much.",
+
+    ["inventory"] = "Property storage",
+
+    -- WARDROBE
+    ["select_outfit"] = "Select outfit",
+    ["equip_outfit"] = "Equip outfit",
+    ["delete_outfit"] = "Delete outfit",
+    ["confirm_delete_outfit"] = "Delete outfit %s?",
+    ["no_outfits"] = "You don't have any outfits",
+
+    -- FURNISH
+    ["furnishing"] = [[
+Speed: %s ~INPUT_COVER~ ~INPUT_TALK~
+Use ~INPUT_WEAPON_WHEEL_PREV~ ~INPUT_WEAPON_WHEEL_NEXT~ to move the furniture up/down
+Use ~INPUT_ATTACK~ ~INPUT_AIM~ to rotate the object (rotation: %s)
+Use ~INPUT_CELLPHONE_LEFT~ ~INPUT_CELLPHONE_RIGHT~ ~INPUT_CELLPHONE_UP~ ~INPUT_CELLPHONE_DOWN~ to move the object
+Press ~INPUT_DETONATE~ to teleport the object to you
+Press ~INPUT_FRONTEND_RDOWN~ to confirm
+Press ~INPUT_FRONTEND_RRIGHT~ to cancel
+]],
+    ["placing_furniture"] = "Placing furniture",
+
+    -- PURCHASE HOUSE NOTIFICATIONS & LOADING
+    ["owns_max"] = "You own the maximum amount of properties and can therefore not buy another one.",
+    ["processing_payment"] = "Processing transaction",
+
+    -- PREVIEW
+    ["previewing"] = [[
+~h~Previewing interiors~h~ (%i/%i)
+
+Previewing interior %s
+Use ~INPUT_CELLPHONE_LEFT~ ~INPUT_CELLPHONE_RIGHT~ to preview other interiors
+Press ~INPUT_FRONTEND_RRIGHT~ to stop previewing.
+]],
+    ["previewing_nohelp"] = [[
+~h~Previewing interior~h~
+
+Press ~INPUT_FRONTEND_RRIGHT~ to stop previewing.
+]],
+    ["preview_property"] = "Preview interior",
+    ["select_interior"] = "Select an interior",
+
+    -- KEY
+    ["key_label"] = "%s #%i [%s]",
+    ["not_nearby"] = "You must be at the door to lock / unlock.",
+
+    ["locked_apart"] = "You ~r~locked~s~ the apartment",
+    ["unlocked_apart"] = "You ~g~unlocked~s~ the apartment",
+
+    ["locked_house"] = "You ~r~locked~s~ the house",
+    ["unlocked_house"] = "You ~g~unlocked~s~ the house",
+
+    -- FURNITURE STORE
+    ["furniture_blip"] = "Furniture store",
+    ["furniture_marker"] = "Browse furniture",
+
+    ["furniture_is_storage"] = "\nYou can store items in this piece of furniture",
+    ["furniture_browsing"] = [[
+Browsing ~r~%s~s~%s
+Use ~INPUT_REPLAY_BACK~ ~INPUT_REPLAY_ADVANCE~ to change item (%i/%i)
+Use ~INPUT_REPLAY_FFWD~ ~INPUT_REPLAY_REWIND~ to change category (%i/%i)
+Use ~INPUT_MOVE_LEFT_ONLY~ ~INPUT_MOVE_RIGHT_ONLY~ to rotate the object
+Use ~INPUT_CURSOR_SCROLL_UP~ ~INPUT_CURSOR_SCROLL_DOWN~ to zoom in / out
+Use ~INPUT_MOVE_UP_ONLY~ ~INPUT_MOVE_DOWN_ONLY~ to tilt the camera up / down
+Press ~INPUT_FRONTEND_RDOWN~ to buy ~b~%s~s~ for ~g~$%s~s~.
+Press ~INPUT_FRONTEND_RRIGHT~ to exit the furniture store]],
+    ["furniture_confirm"] = [[
+Purchase ~b~%s~s~ for ~g~$%s~s~?
+~INPUT_FRONTEND_RDOWN~ Purchase
+~INPUT_FRONTEND_RRIGHT~ Cancel
+]],
+    ["loading_object"] = "Loading object",
+    ["couldnt_load"] = "Could not load the object (%s)",
+    ["furniture_purchase_sell"] = "Furniture store",
+    ["purchase_furniture"] = "Browse furniture",
+    ["resell_furniture"] = "Sell furniture",
+    ["fetching_furniture"] = "Fetching owned furniture",
+    ["no_furniture"] = "You don't have any furniture.",
+    ["your_furniture"] = "Your furniture",
+    ["owned_furniture"] = "%s x%i",
+    ["selling_furniture"] = "Selling furniture",
+    ["sold_furniture"] = "You sold x%i %s for $%i",
+    ["sell_one"] = "Sell one for $%i",
+    ["sell_all"] = "Sell all for $%i",
+    ["purchased_furniture"] = "You purchased %s for $%i",
+
+    -- GENERAL TEXT
+    ["yes"] = "Yes",
+    ["no"] = "No",
+    ["locked"] = "Locked",
+    ["unlocked"] = "Unlocked",
+    
+    ["spawn_apart"] = "Apartment #%i",
+    ["spawn_house"] = "House #%i",
+
+    ["contact_realtor"] = "Contact a realtor to purchase this property",
+
+    -- LOGS
+    ["LOG_backed_up"] = "Backed up %s's property (#%i, id %s).\n\nFile location: %s",
+    ["LOG_purchase"] = "Purchased property #%i for $%i",
+    ["LOG_sell"] = "Sold property #%i for $%i",
+    ["LOG_deposit"] = "Deposited x%i %s",
+    ["LOG_withdrew"] = "Withdrew x%i %s",
+}
+
+-- ignore this
+setmetatable(Strings, {__index = function(self, key)
+    print("NO KEY", key)
+    return "Error: Missing translation for \""..key.."\""
+end})
